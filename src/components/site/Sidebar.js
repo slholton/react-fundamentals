2796f9dfd14420b99245210623c8d313054a2edc
Shavonne Holton <shavonne.holton@gmail.com>
2021-06-30 12:41:43 -0700

    updated the stopwatch

--- src/components/site/Sidebar.js
+++ src/components/site/Sidebar.js
@@ -12,6 +12,7 @@ import State from '../concepts/State';
 import Effects from '../concepts/Effects'
 import PropsDemo from '../concepts/PropsDemo';
 import Hooks from '../concepts/Hooks';
+import TimePiecesApp from '../apps/timer-apps/TimePiecesApp';
 
 const Sidebar = () => {
     return(
@@ -26,6 +27,7 @@ const Sidebar = () => {
                     <li><Link to='/effects'>Effects</Link></li>
                     <li><Link to='/propsdemo'>Props Demo</Link></li>
                     <li><Link to='/hooks'>Hooks</Link></li>
+                    <li><Link to='/timer'>Timers</Link></li>
                 </ul>
             </div>
             <div className='sidebar-route'>
@@ -39,6 +41,7 @@ const Sidebar = () => {
                     <Route exact path='/effects'><Effects /></Route>
                     <Route exact path='/propsdemo'><PropsDemo /></Route>
                     <Route exact path='/hooks'><Hooks /></Route>
+                    <Route exact path='/timer'><TimePiecesApp /></Route>
                 </Switch>
             </div>
         </div>
